d676457f77156fd7205a360b5084387a139b89ab
rohitkolankar <rohitkolankar7@gmail.com>
2020-12-04 14:17:00 +0530

    Changes App.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const txtinput = document.querySelector("myText");
-console.log(txtinput);
+const txtinput = document.querySelector("#myText");
+const transBtn = document.querySelector("#btnTranslate");
+const outputDiv = document.querySelector("#output");
+
+
+
+
+function clickHandler() {
+    console.log(txtinput.value);
+    outputDiv.innerText = "TRanslated Text";
+}
+
+transBtn.addEventListener("click", clickHandler);
+//console.log(txtinput);
