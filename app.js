effc1613424b07b400c52956d68f20b6c2fb5392
rohitkolankar <rohitkolankar7@gmail.com>
2020-12-07 22:31:26 +0530

    Done

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const txtinput = document.querySelector("#myText");
-const transBtn = document.querySelector("#btnTranslate");
-const outputDiv = document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
 
 function clickHandler() {
-    console.log(txtinput.value);
-    outputDiv.innerText = "TRanslated Text";
-}
+    var inputText = txtInput.value; // taking input
 
-transBtn.addEventListener("click", clickHandler);
-//console.log(txtinput);
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler);
